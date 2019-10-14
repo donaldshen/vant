510b4f07443d8051a8055cd0f5d7232923e49ed3
levy <897895407@qq.com>
2019-10-14 16:24:53 +0800

    chore: build site

--- build/release-site.sh
+++ build/release-site.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
-rm -rf docs/dist
+set -e
+#rm -rf docs/dist
 
 npx cross-env NODE_ENV=production webpack --config build/webpack.site.prd.js
 
-superman-cdn /vant ./docs/dist/*.js
-
-rm -rf docs/dist/*.js
-
-gh-pages -d docs/dist --add
+#superman-cdn /vant ./docs/dist/*.js
+#
+#rm -rf docs/dist/*.js
+#
+#gh-pages -d docs/dist --add
